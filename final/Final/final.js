b63d96a818fb3fd3428fa6721557d2b799d6cdb7
Guntu Venugopal <guntugordon@gmail.com>
2025-04-11 18:39:02 -1000

    fials

--- final/Final/final.js
+++ final/Final/final.js
@@ -1,36 +1,43 @@
-document.getElementById("myform").addEventListener('submit',function(event){
+document.getElementById( "myform").addEventListener('submit', function(event) {
     event.preventDefault();
-    
- 
 
-   
-     const fullname = document.getElementById('fullname').value;
-     const email = document.getElementById('email').value;
+    const Fullname = document.getElementById ("fullname").value;
+    const email = document.getElementById("email").value;
     
 
-
-     
-
-        const formdata = {
-        fullname: fullname,
-        email: email,
-
-
-
-     }
-    console.log(formData); 
-    const xhr = new XMLHttpRequest();
-    xhr.open("Get", "submit.json", true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onreadystatechange = function () {
-        if ( xhr.readystate === 4 && xhr.status === 200){
-            alert ( 'Form submitted successfully.');
-        }   else if ( xhr.readyState === 4) {
-            alert( 'Error submitting form.') ;
-        }                
+    if ( !Fullname ){
+        alert("Please provide a fullname");
+        return;
     }
-    xhr.send( JSON.stringify( formdata));
+
+    if ( !email){
+        alert( "Please enter your email");
+        return;
+    }
 
 
+
+
+const data = {
+    Fullname: fullname,
+    email: email,
+    
+}
+
+const xhr = new XMLHttpRequest ();
+xhr.open ("GET", "submit.json", true );
+xhr.setRequestHeader ("Content-type", "application/json;charset=UTF-8");
+xhr.onreadystatechange = function (){
+    if ( xhr. readyState === 4 && xhr.status === 200) {
+    const response = JSON.parse(xhr.responseText);
+    alert("Success");
+    document.getElementById("Message").innerHTML = response.message;
+    document.getElementById("myform").innerHTML = "";
+    } else if ( xhr.readystate ===4){
+        alert('error submitting form.');
+    }
+};
+xhr.send(JSON.stringify(data));
+console.log(data);
 
 });
